df429ca8995eb614ffa837f847a2b5483d901812
YaqubAliyev4513 <51742533+YaqubAliyev4513@users.noreply.github.com>
2023-01-31 02:31:47 +0400

    first steps
    Creating Launcher.Exercise Controller and also fxml file

---
+++ src/az/developia/main/LauncherClass.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package az.developia.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author user
+ */
+public class LauncherClass extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getResource("resources/index.fxml"));
+       stage.setTitle("To Do List project(by DEA Instructor Yaqub Aliyev)");
+       stage.setScene(new Scene(root,300,275));
+       stage.show();
+    }
+    
+}
